822a11c034784202d583822a73d6283a5af10dae
kotoku-code <kennedy.kotoku@gmail.com>
2020-08-04 15:23:50 +0200

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'links#index'
+
+  resources :links, except: :index do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+    post :upvote, on: :member
+    post :downvote, on: :member
+  end
+
+  get '/comments' => 'comments#index'
+  get '/newest' => 'links#newest', as: :newest_links
+
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
+
+  resources :users, only: [:new, :create]
 end
